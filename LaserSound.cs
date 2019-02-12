64f6aded9bd4d6dff668c4dfb1d3bc9e3ffada72
maiix0ne <43549956+maiix0ne@users.noreply.github.com>
2019-02-11 20:25:45 -0500

    Add files via upload

---
+++ LaserSound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserSound : MonoBehaviour
+{
+
+    public AudioClip Laser;
+    private LaserSound source;
+
+  
+    void Awake()
+    {
+        source = GetComponent<LaserSound>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        LaserSound LaserSound = other.gameObject.GetComponent<LaserSound>();
+    }
+
+}
